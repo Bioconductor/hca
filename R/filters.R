b6d5d627fa378695f3bf191a899c9cbd33837117
MA46029 <maya.mcdaniel@roswellpark.org>
2021-01-07 16:52:14 -0500

    incorporating some comments

--- R/filters.R
+++ R/filters.R
@@ -5,7 +5,23 @@
 #' @description This set of functions takes user input to be used as query
 #' filters, ideally in the form of nested lists. This input is then validated,
 #' reformatted to JSON, and encoded into a properly formatted URL.
-#'
+
+## other helper functions
+.filters_validate <- function(x) {
+    ## print(names(x))
+    stopifnot(
+        ## 'filter' must be a list
+        is.list(x),
+        ## length 0 or elements must have names
+        length(x) == 0L || !is.null(names(x))
+        ## each element in the list must also be a list
+        ## appropriate verbs only
+        ## i.e must be one of "is", "within", "intersects", or "contains"
+    )
+
+    x
+}
+
 #' @param ... named arguments, each of which is a `list()` specifying a query
 #' facet and its corresponding value to be used in the query
 #'
@@ -33,8 +49,14 @@
 #' @importFrom utils URLencode
 #'
 #' @export
-filters <- function(...){
+filters <- function(...) {
     filters_list <- list(...)
+
+    ## make sure zero-length filters have names
+    if (length(filters_list) == 0L) {
+        names(filters_list) <- character()
+    }
+
     ## validate list of filters
     valid_filters <- .filters_validate(filters_list)
 
@@ -42,9 +64,9 @@ filters <- function(...){
     ## do elements of `filters` need to be wrapped in `list()`?
     ## ??? filters <- lapply(filters list) ???
     json <- toJSON(valid_filters)
-    URLencode(json, reserved = TRUE)
+    encoded <- URLencode(json, reserved = TRUE)
 
-    result <- list(json = json, filters = valid_filters)
+    result <- list(json = encoded, filters = valid_filters)
 
     ## create a 'filters' object (S3 for now)
     ## S3 classes have simple linear inheritance, and `c("filters","HCAccess")`
@@ -61,24 +83,6 @@ filters <- function(...){
 
 .filters_filters <- function(x) x$filters
 
-## other helper functions
-.filters_validate <- function(x) {
-    ## print(names(x))
-    stopifnot(
-        ## 'filter' must be a list
-        is.list(x),
-        ## length 0 or elements must have names
-        length(x) == 0L || !is.null(names(x))
-    )
-
-    ## make sure zero-length filters have names
-    if (length(x) == 0L) {
-        names(x) <- character()
-    }
-
-    x
-}
-
 ## @rdname is used to document more than one function in the same file
 ## the syntax convention for naming class methods is <generic>.<class>
 ## where a generic function is just an intermediary which determines which
@@ -89,7 +93,7 @@ filters <- function(...){
 #' @param x for `length()` and `print()`, an object of class `filters`.
 #'
 #' @export
-length.filters <- function(x){
+length.filters <- function(x) {
     ## length() is a generic which chooses its implementation based on the class
     ## of its input
     length(.filters_filters(x))
@@ -98,7 +102,7 @@ length.filters <- function(x){
 #' @rdname filters
 #'
 #' @export
-print.filters <- function(x, ...){
+print.filters <- function(x, ...) {
     ## cat() is an internal function which concatenates inputs and prints
     ## print() is also a generic which chooses its implementation based
     ## on the class of its input
