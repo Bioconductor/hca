37fbd425b82f51d41b8c8cd19cce6efc113172b8
MA46029 <maya.mcdaniel@roswellpark.org>
2021-02-05 15:56:00 -0500

    Added next_tbl() and prev_tbl()

--- R/tibbles.R
+++ R/tibbles.R
@@ -5,6 +5,70 @@
     bundles = .BUNDLES_COLUMNS
 )
 
+#' Tibble Functions
+#'
+#' @rdname tibbles
+#'
+#' @name next_tbl
+#'
+#' @param curr_tbl tibble current tibble for which we want the next page
+#'
+#' @return tibble
+#'
+#' @export
+next_tbl <- function (curr_tbl)
+    UseMethod("next_tbl")
+
+#' @importFrom tibble as_tibble
+#' @export
+next_tbl.tbl_hca <- function (curr_tbl) {
+    url <- attr(curr_tbl, "pagination")[["next"]]
+    if (is.null(url)) {
+        print("You are already on the last page of results.")
+        return(url)
+    } else {
+        response <- GET(url)
+        stop_for_status(response)
+        result <- list(
+            content = content(response),
+            status_code = response$status_code
+        )
+
+        return(as_tibble(result))
+    }
+}
+
+#' @rdname tibbles
+#'
+#' @name prev_tbl
+#'
+#' @param curr_tbl tibble current tibble for which we want the previous page
+#'
+#' @return tibble
+#'
+#' @export
+prev_tbl <- function (curr_tbl)
+    UseMethod("prev_tbl")
+
+#' @importFrom tibble as_tibble
+#' @export
+prev_tbl.tbl_hca <- function (curr_tbl) {
+    url <- attr(curr_tbl, "pagination")[["previous"]]
+    if (is.null(url)) {
+        print("You are already on the first page of results.")
+        return(url)
+    } else {
+        response <- GET(url)
+        stop_for_status(response)
+        result <- list(
+            content = content(response),
+            status_code = response$status_code
+        )
+
+        return(as_tibble(result))
+    }
+}
+
 #' @importFrom tibble tibble
 .default_columns <-
     function(
@@ -41,6 +105,7 @@
     attr(hca_tbl, "columns") <- keys
     attr(hca_tbl, "pagination") <- x$pagination
 
-    hca_tbl
+    class(hca_tbl) <- c("tbl_hca", class(tibble()))
 
+    hca_tbl
 }
