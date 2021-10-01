0cb113ebd73a3912f4d6c9b6a15c8c02fcffbc79
Martin Morgan <Martin.Morgan@RoswellPark.org>
2021-10-01 13:22:54 -0400

    manifest() obeys API; don't export 'internal' functions
    - only export manifest() and manifest_cache()
    - document other functions using 'internal' keyword -- document generated
      but not in the user index
    - obey fetch/manifest/files -- 301 means wait, 302 means ready
    - use readr::read_tsv() for fast input
    - add `update_cache =` to optionally update a (stale) cache

--- R/manifest.R
+++ R/manifest.R
@@ -6,29 +6,32 @@
 #'
 #' @title HCA File Querying
 #'
-#' @description `manifest()` takes a list of user provided project titles
-#' to be used to query the HCA API for information about
-#' available manifest files.
+#' @description `manifest()` takes a list of user provided project
+#'     titles to be used to query the HCA API for information about
+#'     available manifest files.
 NULL # don't add next function to documentation
 
-#' @rdname manifest
+#' @rdname manifest-internal
 #'
-#' @description `manifest_url_generator()` takes a filter object with criteria
-#' for the query and the catalog to search within.
+#' @title Internal functions used by `manifest()`
+#'
+#' @description `manifest_url_generator()` takes a filter object with
+#'     criteria for the query and the catalog to search within.
 #'
 #' @param manifest_filter hca filter object
 #'
 #' @param catalog character() name of catalog
 #'
-#' @return `manifest_url_generator()` returns a url string to be used in an
-#' API call
+#' @return `manifest_url_generator()` returns a url string to be used
+#'     in an API call
 #'
-#' @export
+#' @keywords internal
 #'
 #' @examples
 #' manifest_url_generator(filters(), catalogs()[1])
-manifest_url_generator <- function(manifest_filter, catalog){
-
+manifest_url_generator <-
+    function(manifest_filter, catalog)
+{
     stopifnot(
         ## catalog validation
         `catalog must be a character scalar returned by catalogs()` =
@@ -43,30 +46,31 @@ manifest_url_generator <- function(manifest_filter, catalog){
         filters = .filters_encoding(manifest_filter),
         base_path = .MANIFEST_FILE_PATH)
 
-    manifest_uri_construct <- .hca_path(manifest_index_path)
-    manifest_uri_construct
+    .hca_path(manifest_index_path)
 }
 
-#' @rdname manifest
+#' @rdname manifest-internal
 #'
-#' @description `manifest_uuid_constructo()` takes a filter object with criteria
-#' for the query and the catalog to search within.
+#' @description `manifest_uuid_constructor()` takes a filter object
+#'     with criteria for the query and the catalog to search within.
 #'
 #' @param manifest_filter hca filter object
 #'
 #' @param catalog character() name of catalog
 #'
-#' @return `manifest_uuid_constructo()` returns a url string to be used in an
-#' API call
+#' @return `manifest_uuid_constructo()` returns a url string to be
+#'     used in an API call
 #'
 #' @importFrom jsonlite fromJSON
 #' @importFrom digest digest
 #'
-#' @export
+#' @keywords internal
 #'
 #' @examples
-#' manifest_url_generator(filters(), catalogs()[1])
-manifest_uuid_constructor <- function(manifest_filter, catalog) {
+#' manifest_uuid_generator(filters(), catalogs()[1])
+manifest_uuid_constructor <-
+    function(manifest_filter, catalog)
+{
     stopifnot(
         ## catalog validation
         `catalog must be a character scalar returned by catalogs()` =
@@ -92,40 +96,74 @@ manifest_uuid_constructor <- function(manifest_filter, catalog) {
 #' @importFrom httr progress GET stop_for_status content write_disk config
 #' @importFrom BiocFileCache BiocFileCache bfcquery bfcnew bfcrpath
 #' @importFrom tools file_ext
-.single_manifest_download <- function(manifest_id,
-                                      manifest_ref_url,
-                                      base_destination)
+.single_manifest_download <-
+    function(manifest_id, manifest_ref_url, base_destination, update_cache)
 {
-    # creating a BiocFileCache at the specified location
+    ## creating a BiocFileCache at the specified location
     bfc <- BiocFileCache(base_destination, ask = FALSE)
-    # if file is not already in cache, proceed
-    # using url as unique identifier
-    if (!NROW(bfcquery(bfc, manifest_id))) {
-        # do not follow redirect
+    ## if file is not already in cache, use url as unique identifier
+    bfc_query <- bfcquery(bfc, manifest_id)
+    if (update_cache || !NROW(bfc_query)) {
+        ## do not follow redirect
         config <-  httr::config(followlocation = 0)
-        ref_response <- httr::GET(manifest_ref_url, config = config)
 
-        # location of file for download
-        download_url <- httr::content(ref_response)$Location
+        total_wait <- 0L
+        message("requesting manifest")
+        repeat {
+            ref_response <- httr::GET(manifest_ref_url, config = config)
+            stop_for_status(ref_response)
 
+            content <- httr::content(ref_response)
+            status <- content$Status
+            if (identical(status, 302L))
+                break
+            else if (identical(status, 301L)) {
+                wait <- content$`Retry-After`
+                total_wait <- total_wait + wait
+                manifest_ref_url <- content$Location
+                message(
+                    "waiting ", wait, " more seconds ",
+                "(", total_wait, " seconds total)..."
+                )
+                Sys.sleep(wait)
+            } else {
+                stop(
+                    "unexpected response generating manifest:",
+                    "\n  Status: ", status
+                )
+            }
+        }
+
+        ## location of file for download
+        download_url <- content$Location
         extension <- ".tsv"
-
-        savepath <- bfcnew(bfc, manifest_id, ext = extension)
+        if (NROW(bfc_query)) {
+            savepath <- bfc_query$rpath
+        } else {
+            savepath <- bfcnew(bfc, manifest_id, ext = extension)
+        }
         manifest_response <- GET(
             download_url, write_disk(savepath, overwrite = TRUE),
-            # if running interactively, show file download progress bar
+            ## if running interactively, show file download progress bar
             if (interactive()) progress()
         )
+        stop_for_status(manifest_response)
     }
-    # return the path to the file's location in the cache
+
+    ## return the path to the file's location in the cache
     unname(bfcrpath(bfc, manifest_id))
 }
 
+#' @rdname manifest
+#'
 #' @param filters hca filter object
 #'
 #' @param catalog character() name of catalog
 #'
-#' @importFrom utils read.csv
+#' @param update_cache logical(1) when `TRUE`, update an existing
+#'     cached resource by querying the HCA data server.
+#'
+#' @importFrom readr read_tsv
 #' @importFrom tibble as_tibble
 #'
 #' @examples
@@ -133,10 +171,15 @@ manifest_uuid_constructor <- function(manifest_filter, catalog) {
 #'     projectId = list(is = "4a95101c-9ffc-4f30-a809-f04518a23803"),
 #'     fileFormat = list(is = "loom"),
 #'     workflow = list(is = c("optimus_v4.2.2", "optimus_v4.2.3"))
-#'     )
-#' \dontrun{manifest(manifest_filter)}
+#' )
+#' \dontrun{
+#' result <- manifest(manifest_filter)
+#' result
+#' }
 #' @export
-manifest <- function(filters = NULL, catalog = NULL) {
+manifest <-
+    function(filters = NULL, catalog = NULL, update_cache = FALSE)
+{
 
     if (is.null(filters)){
         filters <- filters()
@@ -145,19 +188,32 @@ manifest <- function(filters = NULL, catalog = NULL) {
     if (is.null(catalog)){
         catalog <- catalogs()[1]
     }
+    stopifnot(
+        "`filters` must be NULL or the result of a call to `filters()`" =
+            inherits(filters, "filters"),
+        "`catalog` must be character(1) matching a value in `catalogs()`" =
+            .is_scalar_character(catalog) && (catalog %in% catalogs()),
+        .is_scalar_logical(update_cache)
+    )
 
-    manifest_uri <- manifest_url_generator(manifest_filter = filters,
-                                           catalog = catalog)
+    manifest_uri <- manifest_url_generator(
+        manifest_filter = filters,
+        catalog = catalog
+    )
 
-    manifest_uuid <- manifest_uuid_constructor(manifest_filter = filters,
-                                               catalog = catalog)
+    manifest_uuid <- manifest_uuid_constructor(
+        manifest_filter = filters,
+        catalog = catalog
+    )
 
-    manifest_file_location <- .single_manifest_download(manifest_id = manifest_uuid,
-                                                        manifest_ref_url = manifest_uri,
-                                                        base_destination = manifest_cache(create = TRUE))
+    manifest_file_location <- .single_manifest_download(
+        manifest_id = manifest_uuid,
+        manifest_ref_url = manifest_uri,
+        base_destination = manifest_cache(create = TRUE),
+        update_cache = update_cache
+    )
 
-    manifest_tibble <- read.csv(manifest_file_location, sep = "\t") %>%
-        tibble::as_tibble()
+    manifest_tibble <- read_tsv(manifest_file_location, show_col_types = FALSE)
 
     ## make returned tbl extend tbl_df not tbl_hca
     ## i.e. manifest_tbl_hca tbl_df ...
@@ -185,7 +241,9 @@ manifest <- function(filters = NULL, catalog = NULL) {
 #'
 #' @examples
 #' manifest_cache(create = FALSE)
-manifest_cache <- function(create = FALSE) {
+manifest_cache <-
+    function(create = FALSE)
+{
     stopifnot(.is_scalar_logical(create))
 
     cache <- R_user_dir(package = "hca", which = "cache")
