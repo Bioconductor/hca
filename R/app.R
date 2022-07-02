d28b8bf1c603c12dc84aaafe004b397b4ef0507b
Martin Morgan <Martin.Morgan@RoswellPark.org>
2022-07-02 16:59:38 -0400

    necessary changes
    - Import: shiny, DT in DESCRIPTION, relevant functions in NAMESPACE
      (via roxygen2 markup)
    - invoke `runGadget()` with named arguments (`=` rather than `<-`)
    - formatting and whitespace cleanup

--- R/app.R
+++ R/app.R
@@ -1,35 +1,41 @@
-library(DT)
-library(shiny)
+#' @importFrom shiny runGadget basicPage textOutput actionButton h2 hr
+#'
+#' @importFrom DT renderDataTable datatable dataTableOutput
+#'
+#' @export
+hca_view <-
+    function(tbl)
+{
+    runGadget(
+        app = basicPage(
+            h2("The HCA data"),
+            textOutput("data_selected", inline = TRUE),
+            actionButton("done", "Done"),
+            hr(),
+            dataTableOutput("hca")
+        ),
 
-hca_view <- 
-    function(tbl) {
-        runGadget(
-            ui <- basicPage(
-                h2("The HCA data"),
-                textOutput("data_selected", inline = TRUE),
-                actionButton("done", "Done"),
-                hr(),
-                DT::dataTableOutput("hca")
-            ),
-            
-            server <- function (input, output) {
-                output$hca = DT::renderDataTable({
-                    DT::datatable(tbl,
-                                  selection = 'multiple',
-                                  rownames = FALSE
-                    )
-                })
-                output$data_selected <- renderText({
-                    paste(
-                        "Number selected:",
-                        length(input$hca_rows_selected)
-                    )
-                })
-                observe({
-                    if(input$done) {
-                        stopApp(tbl[input$hca_rows_selected,])
-                    }
-                })
-            }
-        )
-    }
+        server = function (input, output) {
+            output$hca = renderDataTable({
+                datatable(
+                    tbl,
+                    selection = 'multiple',
+                    rownames = FALSE
+                )
+            })
+
+            output$data_selected <- renderText({
+                paste(
+                    "Number selected:",
+                    length(input$hca_rows_selected)
+                )
+            })
+
+            observe({
+                if(input$done) {
+                    stopApp(tbl[input$hca_rows_selected,])
+                }
+            })
+        }
+    )
+}
